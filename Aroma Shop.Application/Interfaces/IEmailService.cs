ffb0a1e2089f45a0768bd72372da1a0057380624
mohammadt <mohmmadt1379@gmail.com>
2025-08-20 12:42:56 +0330

    Revert "first new way"
    This reverts commit 2e2ba8d451b3aca2951d98f44b77d94097b3ebd4.

--- Aroma Shop.Application/Interfaces/IEmailService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Aroma_Shop.Application.Interfaces
-{
-    public interface IEmailService
-    {
-        public Task SendEmailAsync(string toEmail, string subject, string message, bool isMessageHtml = false);
-    }
-}
